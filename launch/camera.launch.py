7dfb4a30597105fb68c04324d0ba884fb4811bba
Thomas Schmiereck <thomas@schmiereck.de>
2026-02-01 22:56:16 +0100

    fix(ROS2): Update camera launch parameters and project info for improved topic publishing

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -10,7 +10,14 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 #'video_device': '/dev/video0',
-                'video_device': '/dev/video13',  # ← ISP statt Raw-Sensor!
+                # Du brauchst ein Device mit :capture: UND YUYV-Unterstützung:
+                # Device        Typ                 YUYV?           Empfehlung
+                # /dev/video0   unicam capture      ✓               ❌ Raw-Sensor (funktioniert nicht)
+                # /dev/video14  bcm2835-isp capture ✓ RGB3, YUYV    ✅ BESTE WAHL
+                # /dev/video15  bcm2835-isp capture ✓ YUYV          ✅ Alternative
+                # /dev/video21  bcm2835-isp capture ✓ RGB3, YUYV    ✅ Alternative
+                # /dev/video22  bcm2835-isp capture ✓ YUYV          ✅ Alternative
+                'video_device': '/dev/video14',  # ← ISP statt Raw-Sensor!
                 'image_size': [640, 480],
                 'pixel_format': 'YUYV',
                 'camera_frame_id': 'camera_link',
