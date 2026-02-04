cef5eb0e5804ee3e475ff3715caf7ab6359ea050
Thomas Schmiereck <thomas@schmiereck.de>
2026-02-04 15:17:06 +0100

    feat(debug): Temporarily comment out FPV and RobotLight in GUIServer.py to isolate camera crash

--- Server/GUIServer.py
+++ Server/GUIServer.py
@@ -6,11 +6,11 @@ import signal
 import argparse
 import os
 
-import FPV
+# import FPV # Temporarily commented out for camera debugging
 import psutil
 import Move as move
 import Switch as switch
-import RobotLight as robotLight
+# import RobotLight as robotLight # Temporarily commented out for LED debugging
 import ast
 
 
